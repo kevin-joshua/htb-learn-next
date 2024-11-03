1bb91367f85f2448fcf0eed8bbfb4e328510bfa4
kevin-joshua <kevinajijoshua@gmail.com>
2024-11-03 22:10:06 +0530

    Created a new page 'mypage' and added link in index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import Image from "next/image";
 import localFont from "next/font/local";
+import Link from 'next/link';
+
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -17,6 +19,11 @@ export default function Home() {
     <div
       className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
     >
+      <button className="bg-purple-800 p-4 rounded-xl">
+  <Link href="/mypage">
+    New page link
+  </Link>
+</button>
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
---
+++ pages/mypage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function mypage() {
+  return (
+    <div>
+      Hello, Kevin created this page
+    </div>
+  )
+}
+
+export default mypage
